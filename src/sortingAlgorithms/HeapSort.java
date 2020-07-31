1baf5a6bebf9777885262807b1ef0e3c214cd0ac
Jamie <jamiewong63@googlemail.com>
2020-07-31 09:44:00 +0100

    refactor heap sort

--- src/sortingAlgorithms/HeapSort.java
+++ src/sortingAlgorithms/HeapSort.java
@@ -17,7 +17,7 @@ public class HeapSort {
 	public void sort(int arr[]) {
 		int n = arr.length;
 		
-		// Build heap (rearrange array)
+		// Build heap 
 		for (int i = (n / 2) - 1; i >= 0; i--) {
 			heapify(arr, n, i);
 		}
